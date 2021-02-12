c50b305123f19a72b0f4ec16a3cf5cbd17ef1e34
SaishaMishra <74277521+SaishaMishra@users.noreply.github.com>
2021-02-11 20:06:42 -0600

    Update stone.js

--- js/stone.js
+++ js/stone.js
@@ -11,13 +11,13 @@ class stone
         this.image=loadImage("images/stone.png")
         this.body = Bodies.circle(x, y,r/2, options);
         World.add(world, this.body)
-        this.radius=r;
+        this.r=r;
     }
     display(){
         push();
         translate(this.body.position.x, this.body.position.y);
         imageMode(CENTER);
-        image(this.image, 0, 0, this.radius,this.radius);
+        image(this.image, 0, 0, this.r, this.r);
         pop();
 }
 }
